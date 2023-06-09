5d7d2f00f7d808045dfc461cadccce5519e7f864
oratta <oratta@gmail.com>
2023-06-09 19:55:09 +0900

    be module

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-keyword = "your-keyword"
-tweet_content = get_chatgpt_response(keyword)
-post_tweet(tweet_content)
+from chatgpt import get_chatgpt_response
+from twitter import post_tweet
+
+def main():
+    keyword = "your-keyword"
+    tweet_content = get_chatgpt_response(keyword)
+    post_tweet(tweet_content)
+
+if __name__ == "__main__":
+    main()
