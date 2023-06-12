4b844998a7d9163a21f89b5139de04a15f9b4e87
oratta <oratta@gmail.com>
2023-06-12 20:50:20 +0900

    文章作成時の引数追加

--- chatgpt.py
+++ chatgpt.py
@@ -5,12 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-def get_chatgpt_response(keyword):
+def get_chatgpt_response(keyword1, keyword2):
     response = openai.ChatCompletion.create(
       model="gpt-3.5-turbo",
       messages=[
             {"role": "system", "content": "あなたは10万人のフォロワーを抱える、人気Twitterユーザです。"},
-            {"role": "user", "content": f"100字以内で{keyword}に関して、リツイートされそうなツイート文を作成してください."},
+            {"role": "user", "content": f"100字以内で{keyword1}と{keyword2}を関連付けて、いいねされそうなツイート文を作成してください."},
         ]
     )
 
