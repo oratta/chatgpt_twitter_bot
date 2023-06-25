3eb69a57d490a4f3225a1be261e64660b04d6ea5
oratta <oratta@gmail.com>
2023-06-25 15:42:04 +0900

    TwitterAPIの変更対応

--- twitter.py
+++ twitter.py
@@ -1,14 +1,26 @@
 import tweepy
+import os
+from dotenv import load_dotenv
 
 def post_tweet(content):
+    load_dotenv()
     consumer_key = os.getenv('TWITTER_CONSUMER_KEY')
     consumer_secret = os.getenv('TWITTER_CONSUMER_SECRET')
     access_token = os.getenv('TWITTER_ACCESS_TOKEN')
     access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
+    bearer_token = os.getenv('TWITTER_BEARER_TOKEN')
 
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    client = tweepy.Client(bearer_token=bearer_token,
+                           consumer_key=consumer_key,
+                           consumer_secret=consumer_secret,
+                           access_token=access_token,
+                           access_token_secret=access_token_secret)
 
-    api = tweepy.API(auth)
+    client.create_tweet(text=content)
 
-    api.update_status(content)
+    # auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    # auth.set_access_token(access_token, access_token_secret)
+    #
+    # api = tweepy.API(auth)
+    #
+    # api.update_status(content)
