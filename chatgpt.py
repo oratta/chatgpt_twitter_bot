f9926254818ff436dc346553a4248c60d4c9d8d7
oratta <oratta@gmail.com>
2023-06-25 15:41:47 +0900

    プロンプト修正

--- chatgpt.py
+++ chatgpt.py
@@ -5,13 +5,19 @@ from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-def get_chatgpt_response(keyword1, keyword2):
+
+def get_chatgpt_response(keyword1="", keyword2=""):
+    # messages = [
+    #     {"role": "system", "content": "あなたは10万人のフォロワーを抱える、人気Twitterユーザです。"},
+    #     {"role": "user", "content": f"100字以内で{keyword1}と{keyword2}を関連付けて、いいねされそうなツイート文を作成してください."},
+    # ]
+    messages = [
+        {"role": "system", "content": "あなたは臨床心理士として子育て世代のカウンセリングをしています。"},
+        {"role": "user", "content": f"280字以内で幼い子供を持つ母親に対して、今日1日を労い、明日も子育てを頑張ろうと思えるようなツイート文を作成してください."},
+    ]
     response = openai.ChatCompletion.create(
-      model="gpt-3.5-turbo",
-      messages=[
-            {"role": "system", "content": "あなたは10万人のフォロワーを抱える、人気Twitterユーザです。"},
-            {"role": "user", "content": f"100字以内で{keyword1}と{keyword2}を関連付けて、いいねされそうなツイート文を作成してください."},
-        ]
+        model="gpt-3.5-turbo",
+        messages=messages
     )
 
     return response['choices'][0]['message']['content']
